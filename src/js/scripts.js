e938d4eaa38f18ee45590eef62f60c3d396e4da4
Dorian <dorianoriginaldesings@gmail.com>
2021-08-13 23:17:38 +0200

    Menu responsive y relleno de demostración

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,9 +2,12 @@ const mainNav = document.getElementById('main-nav');
 const toggleMenu = document.getElementById('toggle-menu');
 const toggleBar = document.getElementById('toggle-bar');
 const headerElement = document.getElementById('header');
+const mainElement = document.getElementById('main');
 
 toggleMenu.addEventListener('click', () => {
   toggleBar.classList.toggle('active');
   mainNav.classList.toggle('main-nav--show');
-  header.classList.toggle('header--blur');
+  header.classList.toggle('blur');
+  main.classList.toggle('blur');
+  document.body.classList.toggle('no-scroll');
 });
